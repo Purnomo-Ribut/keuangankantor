3c5621ac95f68fcb3e77213531d470e9ff753c2b
Nella Adrisia <adrisia60@gmail.com>
2023-12-10 21:57:15 +0700

    tampilan index

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,9 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{ asset('css/login.css') }}">
   <title>Halaman Login</title>
 </head>
@@ -15,61 +13,72 @@
 <body>
   <div class="container" id="container">
     <div class="form-container sign-up-container">
-      <form action="">
-        <h1>Create Account</h1>
-        <input type="text" placeholder="first name" />
-        <input type="text" placeholder="second name" />
-        <input type="email" placeholder="email" />
-        <input type="number" placeholder="age" />
-        <input type="password" placeholder="Password" name="pswd1" />
-        <input type="password" placeholder="confirm passord" name="pswd2" />
-        <button type="submit" onclick="matchPassword()">Sign Up</button>
+      <form action="" method="post">
+        @csrf
+        <!-- login kanan -->
+        <h1>Login </h1>
+        <span>
+          <!-- Menampilkan pesan kesalahan -->
+          @if ($errors->any())
+          <div class="alert alert-danger">
+
+            @foreach ($errors->all() as $error)
+            <b>{{ $error }}</b>
+            @endforeach
+            </>
+          </div>
+          @endif
+        </span>
+        <!-- input username -->
+        <input id="username" type="text" name="username" placeholder="Masukkan username" required autocomplete="username" autofocus />
+        <!-- input password -->
+        <input id="password" type="password" placeholder="Password" name="password" required autocomplete="current-password">
+        <!-- <a href="#">Forgot your password?</a> -->
+        <button type="submit">Masuk</button>
       </form>
     </div>
 
-    <!-- Login -->
+    <!-- Login kiri -->
     <div class="form-container sign-in-container">
-      <form action=""  method="post">
-      @csrf
+      <form action="" method="post">
+        @csrf
         <h1>Login </h1>
-        <div class="social-container">
+        <!-- <div class="social-container">
           <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
           <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
           <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-        </div>
+        </div> -->
         <span>
-          
-        <!-- Menampilkan pesan kesalahan -->
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                
-                    @foreach ($errors->all() as $error)
-                        <b>{{ $error }}</b>
-                    @endforeach
-                </>
-            </div>
-        @endif
+          <!-- Menampilkan pesan kesalahan -->
+          @if ($errors->any())
+          <div class="alert alert-danger">
+
+            @foreach ($errors->all() as $error)
+            <b>{{ $error }}</b>
+            @endforeach
+            </>
+          </div>
+          @endif
         </span>
+        <!-- input username -->
         <input id="username" type="text" name="username" placeholder="Masukkan username" required autocomplete="username" autofocus />
-        
+        <!-- input password -->
         <input id="password" type="password" placeholder="Password" name="password" required autocomplete="current-password">
-
-
-        <a href="#">Forgot your password?</a>
-        <button type="submit" >Sign In</button>
+        <!-- <a href="#">Forgot your password?</a> -->
+        <button type="submit">Masuk</button>
       </form>
     </div>
     <div class="overlay-container">
       <div class="overlay">
         <div class="overlay-panel overlay-left">
-          <h1>Welcome Back!</h1>
-          <p>To keep connected with us please login with your personal info</p>
-          <button class="ghost" id="signIn">Sign In</button>
+          <h1>Peringatan</h1>
+          <p>Usahakan Jangan Login Pakai Akun Lain ya..</p>
+          <button class="ghost" id="signIn">Kiri</button>
         </div>
         <div class="overlay-panel overlay-right">
-          <h1>Hello, Friend!</h1>
-          <p>Enter your personal details and start journey with us</p>
-          <button class="ghost" id="signUp">Sign Up</button>
+          <h1>Selamat Datang !</h1>
+          <p>Silahkan Login dengan Akun Masing - Masing </p>
+          <button class="ghost" id="signUp">Kanan</button>
         </div>
       </div>
     </div>
